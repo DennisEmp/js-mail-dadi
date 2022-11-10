5c0aeecc6525b6a25f7e8479e33246783e803254
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-10 17:53:45 +0100

    ho provato sia con else che con un altro if

--- MAIL/js/main.js
+++ MAIL/js/main.js
@@ -6,16 +6,34 @@ let check = prompt("inserisci la tua mail");
 
 const arrMailList = ["yari@gmail.com", "romina@gmail.com", "trevor@gmail.com", "francesco@gmail.com", "gianluca@gmail.com"];
 
+let emailtrovata = false;
 
 // check se il prompt è presente nella lista
 
-for (let i = 0; i < (arrMailList.length); i++){
-    if (check == arrMailList[i]) {
-        console.log (`${arrMailList[i]} sei nella lista degli invitati!!`);
+for (let i = 0; i < arrMailList.length; i++){
+    
+    if (check === arrMailList[i]) {
+       emailtrovata = true;
     }
+    console.log (`${arrMailList[i]} sei nella lista degli invitati!!`);
 
-// SE NON SEI NELLA LISTA
+    // else {
+    //     console.log(`Purtoppo non sei nella lista degli invitati.`);
+    //     };
+};
 
-    else if (check !== arrMailList[i]){
-        console.log(`Purtoppo non sei nella lista degli invitati.`)
-    }
+if(emailtrovata === false) {
+    console.log("non sei stato invitato");
+}
+
+
+
+
+
+
+
+// if (emailtrovata === true){
+// }
+
+// // SE NON SEI NELLA LISTA
+
