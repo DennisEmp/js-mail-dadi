ab95960b346685d768c070b67d582a49e4aa75f8
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-10 19:13:05 +0100

    complete 2

--- DADI/js/main.js
+++ DADI/js/main.js
@@ -4,9 +4,7 @@ let numComputer = Math.floor((Math.random)()*6)+ 1;
 if (numUtente > numComputer) {
     console.log("vince l'utente")
 }
-    // } else {
-//     console.log ("vince il computer")
-// } 
+   
 if (numUtente < numComputer) {
     console.log("vince il computer")
 }
@@ -14,9 +12,5 @@ if (numUtente < numComputer) {
 if (numUtente === numComputer) {
     console.log("Pareggio")
 }
-
-
-
-// let numComputer = math.floor(Math.random)*6 + 1;
 
 console.log(numUtente, numComputer);
